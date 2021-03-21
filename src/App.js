36980645193689cfd1ab5c153246d9438f61079f
DrakonMichael <mpkarpov@gmail.com>
2021-03-20 23:20:00 -0500

    Changed home page to logo, added webstorm configs to gitignore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper">
+        <div className="content">
+          <div className="outer-circle">
+            <div className="inner-circle"></div>
+            <span className={"title-text"}>bagel</span>
+            <span className={"subtitle-text"}>coming soon</span>
+          </div>
+
+        </div>
+      </div>
+
+
   );
 }
 
